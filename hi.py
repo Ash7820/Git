817dfbfe9b3f20df176ca955999d0c462975a1dc
Ashokkumargurram <gurramashokk@gmail.com>
2025-06-02 15:56:40 +0530

    intial second file

--- hi.py
+++ hi.py
@@ -1,9 +1,1 @@
-def factorial(n):
-    result = 1
-    for i in range(2, n + 1):
-        result *= i
-    return result
-
-# Example usage
-num = int(input("Enter a number: "))
-print(factorial(num))
+print("hello")
